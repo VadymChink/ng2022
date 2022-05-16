8141f0443d6f47c93a9ca12502405623420f747a
VadymChink <vadim-vsnnschuk@ro.ru>
2022-05-16 15:59:50 +0300

    hw 2

---
+++ src/app/components/index.ts
@@ -0,0 +1,4 @@
+export * from './home-component/home-.component';
+export * from './user-details/user-details.component';
+export * from './user/user.component';
+export * from './users/users.component';
