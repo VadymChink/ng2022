62763ccdc39576256d02ad26bf67caa87b74dc05
VadymChink <vadim-vsnnschuk@ro.ru>
2022-05-16 15:59:50 +0300

    hw 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HomeComponent, UserComponent, UserDetailsComponent, UsersComponent} from './components';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    UserComponent,
+    UserDetailsComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'homePage', component: HomeComponent},
+      {
+        path: 'usersPage', component: UsersComponent,
+        children: [
+          {path: 'userDetails/:id', component: UserDetailsComponent}
+        ]
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
