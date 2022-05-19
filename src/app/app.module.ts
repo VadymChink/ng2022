ca0420c570f61f7f569d68545c46e9366cdb6392
VadymChink <vadim-vsnnschuk@ro.ru>
2022-05-19 17:04:22 +0300

    hw 3.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainLayoutComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
