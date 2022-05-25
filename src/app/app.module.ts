1707905d6be0d2807cba44e7d60554472e980ee8
VadymChink <vadim-vsnnschuk@ro.ru>
2022-05-25 19:41:21 +0300

    hw 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
 import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
+import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { CarsComponent } from './components/cars/cars.component';
+import { CarComponent } from './components/car/car.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HeaderComponent } from './components/header/header.component';
+import {ReactiveFormsModule} from "@angular/forms";
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainLayoutComponent,
+    CarsComponent,
+    CarComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
